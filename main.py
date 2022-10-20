abf50aa17868533d47885ecbb6f07b57518f5627
Thom van der Steenhoven <thomvandersteenhoven@gmail.com>
2022-10-20 11:11:05 +0200

    Implement argument/config parsing and logging

---
+++ main.py
@@ -0,0 +1,49 @@
+from argparse import ArgumentParser, Namespace
+from pathlib import Path
+
+import yaml
+
+from util.Logger import Logger
+
+
+def parse_arguments() -> Namespace:
+    """
+    Register and parse all arguments, returning a Namespace containing all arguments.
+
+    :return: Namespace containing all arguments.
+    """
+    parser: ArgumentParser = ArgumentParser(description="Simulate a Wi-Fi network with video and tactile data on the "
+                                                        "up-link and haptic data on the down-link.")
+    parser.add_argument("-c", nargs=1, default="./config.yml", type=Path,
+                        help="Location of configuration YAML file. Defaults to ./config.yml", required=False,
+                        dest="config_path")
+    parser.add_argument("-o", nargs=1, default="./output", type=Path,
+                        help="Location of output files. Defaults to ./output", required=False,
+                        dest="output_path")
+
+    return parser.parse_args()
+
+
+def parse_config(config_path: Path):
+    """
+    Parse configuration file and return contents.
+
+    :param config_path: Path to configuration file.
+    :return: Contents of configuration file.
+    """
+    with open(config_path, 'r') as file:
+        return yaml.unsafe_load(file)
+
+
+def main():
+    """
+    Run the simulator.
+    """
+    arguments: Namespace = parse_arguments()
+    parse_config(arguments.config_path)
+    
+    Logger(arguments.output_path)
+
+
+if __name__ == '__main__':
+    main()
---
+++ util/Logger.py
@@ -0,0 +1,89 @@
+from enum import Enum, unique
+from pathlib import Path
+from typing import Any
+
+
+@unique
+class Logfiles(Enum):
+    """
+    Enumeration of output files.
+    Files are backwards compatible with original simulator.
+    """
+    ACCESS_COLLISION = "access_collision.txt"
+    AP_INTERARRIVAL = "AP-interarrival.txt"
+    AP_INTERARRIVAL_VID = "AP-interarrival_vid.txt"
+    AP_DELAYS = "APdelays.txt"
+    AP_DELAYS_COLLECT_HA = "APdelaysCollect_HA.txt"
+    CONFIGURATIONS = "configurations.csv"
+    CW_PSI = "cw_psi.csv"
+    DELAY_DL_ISOLATED = "delay-DL-isolated.txt"
+    DELAY_FILE = "delayFile.txt"
+    FRAG_THRESH = "frag_thresh.txt"
+    GLOBAL = "global.txt"
+    GLOBAL_BUFFER = "global_buffer.txt"
+    HAP_DELAY_P = "hap_delay_p.txt"
+    KIN_DELAY_P = "kin_delay_p.txt"
+    LOGGING = "logging.csv"
+    STA_AMPDU = "STA-AMPDU.txt"
+    STA_INTERARRIVAL = "STA-interarrival.txt"
+    STA_INTERARRIVAL_MU_NOVID = "STA-interarrival-MU-novid.txt"
+    STA_INTERARRIVAL_MU_VID = "STA-interarrival-MU-vid.txt"
+    STA_INTERARRIVAL_SU_NOVID = "STA-interarrival-SU-novid.txt"
+    STA_INTERARRIVAL_SU_VID = "STA-interarrival-SU-vid.txt"
+    STA_FILE = "sta_file.txt"
+    STA_DELAYS_COLLECT_HA = "STAdelaysCollect_HA.txt"
+    STA_DELAYS_COLLECT_VI = "STAdelaysCollect_VI.txt"
+    THROUGHPUT_TRACK = "throughput_track.txt"
+    VID_DELAY = "vid_delay.txt"
+    VID_DELAY_P = "vid_delay_p.txt"
+    VID_METADATA_FILE = "vid_metadata_file.txt"
+
+
+class Logger:
+    __instance = None
+
+    def __init__(self, output_dir: str = None):
+        """
+        Return the singleton Logger.
+
+        :param output_dir: If supplied, sets the output directory. Required on first instantiation.
+        """
+        try:
+            self.output_dir: Path = Path(output_dir).resolve()
+            self.output_dir.mkdir(parents=True, exist_ok=True)
+        except AttributeError:
+            try:
+                # output_dir is None
+                self.output_dir: Path = self.output_dir
+            except AttributeError as e:
+                raise AttributeError("output_dir is required on first instantiation of Logger") from e
+
+    def __new__(cls, *args, **kwargs):
+        # Make Logger a singleton so the output directory can be set once
+        if not cls.__instance:
+            cls.__instance = super(Logger, cls).__new__(cls)
+        return cls.__instance
+
+    def log(self, file: Logfiles, message: str, additional_file_parameter: Any = None, newline: bool = True):
+        """
+        Append the message to the specified file.
+        If file is Logfiles.ACCESS_COLLISION, additional_file_parameter is required, otherwise it is ignored.
+
+        :param file: File to log to.
+        :param message: Message to log.
+        :param additional_file_parameter: Extension of basename for ACCESS_COLLISION files.
+        :param newline: If True, print a newline after the message. Default True.
+        """
+        if file == Logfiles.ACCESS_COLLISION:
+            if not additional_file_parameter:
+                raise AttributeError("Additional file parameter is required for logging to access_collision files")
+            else:
+                (basename, extension) = file.value.split(".")
+                filename = f"{basename}_{str(additional_file_parameter)}.{extension}"
+        else:
+            filename = file.value
+
+        with open(self.output_dir / filename, 'a') as f:
+            f.write(message)
+            if newline:
+                f.write('\n')
